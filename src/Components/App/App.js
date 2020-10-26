d5fb9df0b368ebbf8720d70e24056cad7fc9cffd
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-26 17:20:47 -0600

    Move and fix file paths for App Component files.

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {Component} from 'react'
+import {BrowserRouter} from 'react-router-dom'
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state= {
+
+    }
+  }
+  render () {
+    return (
+      <div className="App">
+        <h1>Test</h1>
+      </div>
+    );
+  }
+}
+
+export default App;

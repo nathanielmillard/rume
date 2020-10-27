c6b7f198da92d55c4bf4b7bfd784fa1f5fa8f8a1
Brigette Doelp <brigette.doelp@gmail.com>
2020-10-27 16:46:38 -0600

    Render title of room on page

---
+++ src/Components/FeelingRoom/FeelingRoom.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './FeelingRoom.scss';
+
+const FeelingRoom = () => {
+
+  return (
+    <h1>Feeling Room</h1>
+  )
+}
+
+export default FeelingRoom
---
+++ src/Components/ReflectionForm/ReflectionForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ReflectionForm.scss';
+
+const ReflectionForm = () => {
+
+  return (
+    <h1>Reflection Form</h1>
+  )
+}
+
+export default ReflectionForm

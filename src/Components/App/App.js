1a1f431366de398c844a217b7066b1daad6a66a8
Brigette Doelp <brigette.doelp@gmail.com>
2020-10-27 16:45:48 -0600

    Set up initial nav structure with Router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,31 +1,49 @@
 import './App.scss';
-import {Component} from 'react'
-import {BrowserRouter} from 'react-router-dom'
-import Lobby from '../Lobby/Lobby.js'
-import emptyLogo from '../../Assets/emptyLogo.svg'
+import {Component} from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Lobby from '../Lobby/Lobby.js';
+import ReflectionForm from '../ReflectionForm/ReflectionForm.js';
+import FeelingRoom from '../FeelingRoom/FeelingRoom.js';
+import BreathingRoom from '../BreathingRoom/BreathingRoom.js';
+import emptyLogo from '../../Assets/emptyLogo.svg';
 
 class App extends Component {
   constructor(props){
-    super(props)
+    super(props);
     this.state= {
 
     }
   }
   render () {
     return (
-      <main className="App">
-        <Lobby />
-        <footer>
-          <img
-            className="homeButton"
-            data-test="homeButton"
-            title="Return To Lobby"
-            src={emptyLogo}
-            alt="Navigate back to home page"
-          />
-          <p>Quote of the day</p>
-        </footer>
-      </main>
+      <BrowserRouter >
+        <main className="App">
+          <Switch >
+          <Route path='/reflecting'>
+            <ReflectionForm />
+          </Route>
+          <Route path='/feeling'>
+            <FeelingRoom />
+          </Route>
+          <Route path='/breathing'>
+            <BreathingRoom />
+          </Route>
+          <Route path='/'>
+          <Lobby />
+          </Route>
+          </Switch>
+          <footer>
+            <img
+              className="homeButton"
+              data-test="homeButton"
+              title="Return To Lobby"
+              src={emptyLogo}
+              alt="Navigate back to home page"
+            />
+            <p>Quote of the day</p>
+          </footer>
+        </main>
+      </BrowserRouter>
     );
   }
 }

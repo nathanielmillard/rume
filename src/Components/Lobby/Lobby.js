1ca40220b485d0f3a47afa32a6e78d13e8d6836f
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-27 10:29:55 -0600

    Create basic layout of Lobby rendered elements

--- src/Components/Lobby/Lobby.js
+++ src/Components/Lobby/Lobby.js
@@ -12,7 +12,20 @@ class Lobby extends Component {
   render() {
     return (
       <section>
-        <h1>LOBBY GOES HERE</h1>
+        <h1>Welcome Back, it's TODAY!</h1>
+        <h2>How are you feeling today?</h2>
+        <section>
+          <button className='moodButton'>Sad</button>
+          <button className='moodButton'>Angry</button>
+          <button className='moodButton'>Anxious</button>
+          <button className='moodButton'>Fine</button>
+        </section>
+        <h2>What do you want do do about it?</h2>
+        <nav>
+          <button className='roomButton'>Reflect</button>
+          <button className='roomButton'>Feel</button>
+          <button className='roomButton'>Breathe</button>
+        </nav>
       </section>
     )
   }

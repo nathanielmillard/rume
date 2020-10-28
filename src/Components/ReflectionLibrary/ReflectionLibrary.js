b05bd9277009b50aefb18af1e7d783bcb6f6461a
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-28 15:46:18 -0600

    Convert to class based component that can render cards based on state

--- src/Components/ReflectionLibrary/ReflectionLibrary.js
+++ src/Components/ReflectionLibrary/ReflectionLibrary.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import './ReflectionLibrary.scss'
+import { Component } from 'react';
+import './ReflectionLibrary.scss';
+import { getReflections } from '../../apiCalls.js';
+import ReflectionCard from '../ReflectionCard/ReflectionCard.js'
 
-const ReflectionLibrary = () => {
-
-  return (
-    <h1>The inner machinations of my mind are an enigma</h1>
-  )
+class ReflectionLibrary extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reflections: [],
+      loadingMessage: 'Gathering Your Library',
+      errorMessage: ''
+    }
+  }
+  createCards = () => {
+    const reflectionCards = this.state.reflections.map(reflection => {
+      return (
+        <ReflectionCard
+          key={reflection.id}
+          id={reflection.id}
+          date={reflection.date}
+          mood={reflection.mood}
+          reflection={reflection.reflection}
+        />
+      )
+    })
+    return reflectionCards
+  }
+  async componentDidMount() {
+    const reflections = await getReflections()
+    this.setState( reflections )
+    this.setState( {loadingMessage: ''})
+  }
+  render() {
+    return (
+      <section>
+        { !this.state.reflections.length ? <h1>{this.state.loadingMessage}</h1> : <h1>{this.createCards()}</h1> }
+      </section>
+    )
+  }
 }
 
 export default ReflectionLibrary

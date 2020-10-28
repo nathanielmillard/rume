f6a3cbe987ddbeb4e5a2121eea9a63035d6d4f5c
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-28 10:44:00 -0600

    Create and pass unit tests.

--- src/Components/Lobby/Lobby.test.js
+++ src/Components/Lobby/Lobby.test.js
@@ -1,14 +1,35 @@
 import { render, screen } from '@testing-library/react';
 import Lobby from './Lobby';
-
-test('renders learn react link', () => {
-  render(<Lobby />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-// It should render a welcome greeting
-// It should prompt the user to choose a current mood
-// It should display 4 mood buttons
-// It should prompt the user to navigate to a room
-// It should display 3 room option buttons
+import {MemoryRouter} from 'react-router-dom'
+describe('Lobby', ()=>{
+  describe('Unit Tests', ()=>{
+    test('Should load messages and prompts in Lobby', () => {
+      render(
+        <MemoryRouter>
+          <Lobby />
+        </MemoryRouter>
+      );
+      const welcomeMessage = screen.getByTestId('welcomeMessage');
+      const checkInQuestion = screen.getByTestId('checkInQuestion');
+      const actionPrompt = screen.getByTestId('actionPrompt');
+      expect(welcomeMessage).toBeInTheDocument();
+      expect(checkInQuestion).toBeInTheDocument();
+      expect(actionPrompt).toBeInTheDocument();
+    });
+    test('Should load actionable elements in Lobby', () => {
+      render(
+        <MemoryRouter>
+          <Lobby />
+        </MemoryRouter>
+      );
+      const moodButtonContainer = screen.getByTestId('moodButtonContainer');
+      const linkToReflecting = screen.getByTestId('linkToReflecting');
+      const linkToFeeling = screen.getByTestId('linkToFeeling');
+      const linkToBreathing = screen.getByTestId('linkToBreathing');
+      expect(moodButtonContainer).toBeInTheDocument();
+      expect(linkToReflecting).toBeInTheDocument();
+      expect(linkToFeeling).toBeInTheDocument();
+      expect(linkToBreathing).toBeInTheDocument();
+    });
+  })
+})

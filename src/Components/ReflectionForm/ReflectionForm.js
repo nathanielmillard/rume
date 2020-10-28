65ace27a457962d3a15d6badee102aac024051cf
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-28 11:50:01 -0600

    Create functionality to save input as state.

--- src/Components/ReflectionForm/ReflectionForm.js
+++ src/Components/ReflectionForm/ReflectionForm.js
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types'
 import './ReflectionForm.scss';
 
-const ReflectionForm = () => {
-
-  return (
-    <section className='RelfectionForm'>
+class ReflectionForm extends Component {
+ constructor(props){
+   super(props)
+   this.state={
+     reflection: '',
+   }
+ }
+ keepReflection = (e) => {
+   this.setState({reflection: e.target.value})
+ }
+  render() {
+    return (
+      <section className='RelfectionForm'>
       <form data-testid='reflectionForm'>
-        <h2  className='relfectionPrompt' data-testid='reflectionPrompt'> You're feeling fine, what went well today? </h2>
-        <label>
-          Write your reflection here:
-          <textarea
-            data-testid='reflectionArea'
-            rows='150'
-            cols='45'
-            >
-          </textarea>
-          <button>Save This Relfection</button>
-        </label>
+      <h2  className='relfectionPrompt' data-testid='reflectionPrompt'> You're feeling fine, what went well today? </h2>
+      <label>
+      Write your reflection here:
+      <textarea
+      data-testid='reflectionArea'
+      rows='150'
+      cols='45'
+      onChange={this.keepReflection}
+      >
+      </textarea>
+      <button>Save This Relfection</button>
+      </label>
       </form>
       <Link to='/myreflections'>
-        <button>Show All Reflections</button>
+      <button>Show All Reflections</button>
       </Link>
-    </section>
-  )
+      </section>
+    )
+  }
 }
 
 export default ReflectionForm
+
+ReflectionForm.propTypes = {
+  mood: PropTypes.string.isRequired,
+}

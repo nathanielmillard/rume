77fd4599eb2f81eb7db563d093c5a14db07ea8bd
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-28 13:48:32 -0600

    Change name of ReflectionList to ReflectionLibrary

--- src/Components/ReflectionLibrary/ReflectionLibrary.js
+++ src/Components/ReflectionLibrary/ReflectionLibrary.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './ReflectionLibrary.scss'
 
-const ReflectionList = () => {
+const ReflectionLibrary = () => {
 
   return (
     <h1>The inner machinations of my mind are an enigma</h1>
   )
 }
 
-export default ReflectionList
+export default ReflectionLibrary

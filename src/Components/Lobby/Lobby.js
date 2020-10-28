e6a791125e31696ba81b90f0043198673915ea40
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-28 10:43:50 -0600

    Add data-testids to Lobby.

--- src/Components/Lobby/Lobby.js
+++ src/Components/Lobby/Lobby.js
@@ -13,23 +13,23 @@ class Lobby extends Component {
   render() {
     return (
       <section className='Lobby'>
-        <h1>Welcome Back, it's TODAY!</h1>
-        <h2>How are you feeling today?</h2>
-        <section className='moodContainer'>
+        <h1 data-testid='welcomeMessage'>Welcome Back, it's TODAY!</h1>
+        <h2 data-testid='checkInQuestion'>How are you feeling today?</h2>
+        <section className='moodContainer' data-testid='moodButtonContainer'>
           <button className='moodButton'>Sad</button>
           <button className='moodButton'>Angry</button>
           <button className='moodButton'>Anxious</button>
           <button className='moodButton'>Fine</button>
         </section>
-        <h2>What do you want do do about it?</h2>
+        <h2 data-testid='actionPrompt'>What do you want do do about it?</h2>
         <nav>
-          <Link to='/reflecting'>
+          <Link to='/reflecting' data-testid='linkToReflecting'>
             <button className='roomButton'>Reflect</button>
           </Link>
-          <Link to='/feeling'>
+          <Link to='/feeling' data-testid='linkToFeeling'>
             <button className='roomButton'>Feel</button>
           </Link>
-          <Link to='/breathing'>
+          <Link to='/breathing' data-testid='linkToBreathing'>
             <button className='roomButton'>Breathe</button>
           </Link>
         </nav>

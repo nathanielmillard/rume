2b03678211891958fc6de0e2ad6795d273cd559a
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-28 10:24:12 -0600

    Create and pass first test.

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,10 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('App', ()=>{
+  describe('Unit Tests', ()=>{
+    test('Should render a footer on the App', () => {
+      render(<App />);
+      const homeButton = screen.getByTestId("homeButton");
+      const randomQuote = screen.getByTestId("randomQuote");
+      expect(homeButton).toBeInTheDocument();
+      expect(randomQuote).toBeInTheDocument();
+    });
+
+  })
+})
 
 // A user sees the quote of the day and the emptyLogo

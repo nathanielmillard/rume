235aa88491d920a19d0c45d4e5a145c10b4fac23
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-28 20:28:06 -0600

    Add basic styles and branding to welcome page

--- src/Components/WelcomePage/WelcomePage.js
+++ src/Components/WelcomePage/WelcomePage.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './WelcomePage.scss'
 
 const WelcomePage = () => {
   return (
-    <section>
-      <h1>Hewwo</h1>
-      <Link to='/lobby'>
-        <button>Door To Lobby</button>
-      </Link>
+    <section className="welcomeWrapper">
+      <h1 className="logo">Rume</h1>
+      <h2>Click the door below to enter the lobby</h2>
     </section>
   )
 }
 
 export default WelcomePage
+
+
+// <Link to='/lobby'>
+// <button>Door To Lobby</button>
+// </Link>

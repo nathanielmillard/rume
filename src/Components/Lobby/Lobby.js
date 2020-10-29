8baff2baaf735ad3004757fc974a6b6ff54f0591
Brigette Doelp <brigette.doelp@gmail.com>
2020-10-29 11:02:28 -0600

    src/Components/Lobby/Lobby.js
    Write and use styled component variable for mood buttons

--- src/Components/Lobby/Lobby.js
+++ src/Components/Lobby/Lobby.js
@@ -2,6 +2,17 @@ import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Lobby.scss';
 import PropTypes from 'prop-types'
+import styled from "styled-components";
+
+const MoodButton = styled.button`
+  border-radius: 100px;
+  margin:1vh;
+  width: 15vh;
+  height: 15vh;
+  font-size: 1.5rem;
+  color: #121316;
+  border: .5px solid #121316;
+`
 
 class Lobby extends Component {
   constructor(props) {
@@ -17,10 +28,10 @@ class Lobby extends Component {
         <h1>Welcome Back, it's {new Date().toDateString()}</h1>
         <h2>How are you feeling today?</h2>
         <section className='moodContainer'>
-          <button className='moodButton fine' onClick={this.props.setMood}>Fine</button>
-          <button className='moodButton anxious' onClick={this.props.setMood}>Anxious</button>
-          <button className='moodButton sad' onClick={this.props.setMood}>Sad</button>
-          <button className='moodButton angry' onClick={this.props.setMood}>Angry</button>
+          <MoodButton className='fine' onClick={this.props.setMood}>Fine</MoodButton>
+          <MoodButton className='anxious' onClick={this.props.setMood}>Anxious</MoodButton>
+          <MoodButton className='sad' onClick={this.props.setMood}>Sad</MoodButton>
+          <MoodButton className='angry' onClick={this.props.setMood}>Angry</MoodButton>
         </section>
         <h2>What do you want do do about it?</h2>
         <nav>

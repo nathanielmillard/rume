7123f7ccf35363618447302c642612301e56cf08
Kristi Miller <kristi.ann.miller@gmail.com>
2020-10-29 20:15:07 -0600

    Add conditional rendering to journal prompt display

--- src/Components/ReflectionForm/ReflectionForm.js
+++ src/Components/ReflectionForm/ReflectionForm.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import { postAReflection } from '../../apiCalls.js'
 import './ReflectionForm.scss';
-import { FormWrap } from '../../StyledComponents.js'
+import { FormWrap, RefPrompt } from '../../StyledComponents.js'
 
 class ReflectionForm extends Component {
  constructor(props){
@@ -27,10 +27,15 @@ class ReflectionForm extends Component {
     return (
       <section className='ReflectionForm'>
         <form data-testid='reflectionForm'>
-          <h2  className='reflectionPrompt' data-testid='reflectionPrompt'> You're feeling fine, what went well today? </h2>
+          <h2 className='reflectionPrompt' data-testid='reflectionPrompt'>
+          You're feeling
+          <RefPrompt mood={this.props.mood}>{this.props.mood.toLowerCase()}</RefPrompt></h2>
           <FormWrap mood={this.props.mood}>
             <label className="formLabel">
-              Write your reflection here:
+              {this.props.mood === 'Fine' && 'What went well today?'}
+              {this.props.mood === 'Angry' && `What has been frustrating about today?`}
+              {this.props.mood === 'Anxious' && `What is keeping up space in your mind? Let your thoughts go.`}
+              {this.props.mood === 'Sad' && 'Let your thoughts pour out onto the page.'}
               <textarea
               id='textArea'
               data-testid='reflectionArea'

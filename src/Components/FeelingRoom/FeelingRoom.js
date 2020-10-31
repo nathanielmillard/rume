9558cb6f39704940507c99897a518d9e74e28420
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-31 13:04:03 -0600

    Create conditionals so at to not overlap on code merge in the future.

--- src/Components/FeelingRoom/FeelingRoom.js
+++ src/Components/FeelingRoom/FeelingRoom.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import './FeelingRoom.scss';
 
-const FeelingRoom = () => {
+const FeelingRoom = (props) => {
+  const createFineRoom = () => {
+    //functions to animate go here
+    return (
+      //elements to animate go here
+    )
+  }
+  const createAnxiousRoom = () => {
+    //functions to animate go here
+    return (
+      //elements to animate go here
+    )
+  }
+  const createSadRoom = () => {
+    //functions to animate go here
+    return (
+      //elements to animate go here
+    )
+  }
+  const createAngryRoom = () => {
+    //functions to animate go here
+    return (
+      //elements to animate go here
+    )
+  }
+
+  const chooseRoomMood = () => {
+    if(props.mood === angry) {
+      createAngryRoom()
+    } else if(props.mood === sad) {
+      createSadRoom()
+    } else if(props.mood === anxious){
+      createAnxiousRoom()
+    } else {
+      createFineRoom()
+    }
+  }
 
   return (
-    <h1>Feeling Room</h1>
+    chooseRoomMood()
   )
 }
 

18317ee1b0d5b77659f74221e63900d5fb3f0ac5
Brigette Doelp <brigette.doelp@gmail.com>
2020-11-02 15:54:32 -0700

    Test that a ReflectionCard renders

--- src/Components/ReflectionCard/ReflectionCard.test.js
+++ src/Components/ReflectionCard/ReflectionCard.test.js
@@ -1,10 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import ReflectionCard from './ReflectionCard';
+import { MemoryRouter } from 'react-router-dom'
+
 
 describe('ReflectionCard', ()=>{
   describe('Unit Tests', ()=>{
     test('Should render a ReflectionCard', () => {
-      expect(true).toBe(true)
+      render(
+        <MemoryRouter>
+          <ReflectionCard />
+        </MemoryRouter>
+      )
+      const cardDate = screen.getByTestId("cardDate");
+      const reflectionMood = screen.getByTestId("reflectionMood");
+      const reflectionEntry = screen.getByTestId("reflectionEntry");
+      expect(cardDate).toBeInTheDocument();
+      expect(reflectionMood).toBeInTheDocument();
+      expect(reflectionEntry).toBeInTheDocument();
     });
 
   })

0345c593d401fbb51f20051c793f99f7b0f9cde0
Nathaniel Millard <nathanielmillard@gmail.com>
2020-11-01 22:08:59 -0700

    Create drop styled component.

---
+++ src/Components/FeelingRoom/FeelingRoom-SC.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const DropIcon = styled.div`
+  height: 1vh;
+  width: .5vh;
+  background-color: hsl(237, 33%, 12%);
+  box-shadow: 1px 1px 3px #F3F5F6;
+  /* margin: .75vh; */
+`

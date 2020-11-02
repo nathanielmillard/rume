81e5ee3fce2fa6f88ee567433baae77fbd8ca1fc
Brigette Doelp <brigette.doelp@gmail.com>
2020-11-02 14:30:41 -0700

    Write tests to make sure WelcomePage renders

--- src/Components/WelcomePage/WelcomePage.test.js
+++ src/Components/WelcomePage/WelcomePage.test.js
@@ -3,13 +3,12 @@ import WelcomePage from './WelcomePage';
 
 describe('WelcomePage', () => {
   describe('Unit Tests', () => {
-    test('Should render site title', () => {
-      expect(true).toBe(true)
+    test('Should render Welcome Page', () => {
+      render(<WelcomePage />)
+      const appTitle = screen.getByTestId("appTitle");
+      const appDirections = screen.getByTestId("appDirections")
+      expect(appTitle).toBeInTheDocument();
+      expect(appDirections).toBeInTheDocument();
     });
-
-    test('Should render door icon', () => {
-      expect(true).toBe(true)
-    });
-
   })
 })

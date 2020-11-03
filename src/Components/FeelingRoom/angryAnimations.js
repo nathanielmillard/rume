33f6c9d7ba516156ed54b84b916eeb4e5306eec7
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-03 13:16:57 -0700

    Refactor animation timing

--- src/Components/FeelingRoom/angryAnimations.js
+++ src/Components/FeelingRoom/angryAnimations.js
@@ -2,66 +2,110 @@ import gsap from 'gsap'
 
 export const angryFeelingAnimation = () => {
   let angryTimeline = gsap.timeline({repeat: 10});
-  for (var i = 0; i <= 9; i++) {
+  for (var i = 0; i <= 10; i++) {
     angryTimeline.from(`.square${i}`, {
       duration: .5,
-      y: "-50vh",
+      y: "-60vh",
       display: "flex",
     })
-    .to(`.square${i}`, {
-      duration: 1.75,
-      y: "60vh",
-      ease: "bounce",
+    .from(`.square${i} .childSquare`, {
+      duration: .5,
+      x: "0",
+      y: "0",
       backgroundColor: 'hsl(329, 57%, 25%)',
+      rotate: "random(-45, 45, 2)",
     })
-    .add("shake")
     .to(`.square${i}`, {
-      backgroundColor: 'hsla(329, 57%, 25%)',
-    }, "shake")
-    .to(`.square${i} #part1`, {
-      backgroundColor: 'hsla(329, 57%, 25%)',
-    }, "shake")
-    .to(`.square${i} #part2`, {
-      backgroundColor: 'hsla(329, 57%, 25%)',
-    }, "shake")
-    .to(`.square${i} #part3`, {
-      backgroundColor: 'hsla(329, 57%, 25%)',
-    }, "shake")
-    .to(`.square${i} #part4`, {
-      backgroundColor: 'hsla(329, 57%, 25%)',
-    }, "shake")
-    .add("explode")
-    .to(`.square${i}`, {
-      backgroundColor: 'hsla(225, 10%, 8%, 0%)',
-      duration: .02
-    }, "explode")
-    .to(`.square${i} #part1`, {
-      y: "-=50vh",
-      x: "-=30vh",
-      duration: .5,
-      rotate: "random(-45, 45, 2)",
-      backgroundColor: 'hsla(225, 10%, 8%, 0%)',
-    }, "explode")
-    .to(`.square${i} #part2`, {
-      y: "-=50vh",
-      x: "+=10vh",
-      duration: .5,
-      rotate: "random(-45, 45, 2)",
-      backgroundColor: 'hsla(225, 10%, 8%, 0%)'
-    }, "explode")
-    .to(`.square${i} #part3`, {
-      y: "-=50vh",
-      x: "-=60vh",
-      duration: .5,
-      rotate: "random(-45, 45, 2)",
-      backgroundColor: 'hsla(225, 10%, 8%, 0%)'
-    }, "explode")
-    .to(`.square${i} #part4`, {
-      y: "-=50vh",
-      x: "+=30vh",
-      duration: .5,
-      rotate: "random(-45, 45, 2)",
-      backgroundColor: 'hsla(225, 10%, 8%, 0%)'
-    }, "explode")
+      backgroundColor: 'hsl(329, 57%, 25%)',
+      duration: 1.5,
+      height: "150px",
+      width: "150px",
+      y: "55vh",
+      ease: "bounce"
+     })
+     .add("explode")
+     .to(`.square${i}`, {
+       duration: .2,
+       backgroundColor: 'hsla(329, 57%, 25%, 0%)'
+     }, "explode")
+     .to(`.square${i} #part1`, {
+       duration:.5,
+       y: "-=50vh",
+       x: "-=30vh",
+       backgroundColor: 'hsla(329, 57%, 25%, 0%)',
+     }, "explode")
+     .to(`.square${i} #part2`, {
+       duration:.5,
+       y: "-=50vh",
+       x: "+=10vh",
+       backgroundColor: 'hsla(329, 57%, 25%, 0%)',
+     }, "explode")
+     .to(`.square${i} #part3`, {
+       duration:.5,
+       y: "-=50vh",
+       x: "-=60vh",
+       backgroundColor: 'hsla(329, 57%, 25%, 0%)',
+     }, "explode")
+     .to(`.square${i} #part4`, {
+       duration:.5,
+       y: "-=50vh",
+       x: "+=30vh",
+       backgroundColor: 'hsla(329, 57%, 25%, 0%)',
+     }, "explode")
+    }
   }
-}
+
+export const angryFeelingAnimation2 = () => {
+   let angryTimeline2 = gsap.timeline({repeat: 10, delay: .5});
+   for (var i = 3; i <= 13; i++) {
+     angryTimeline2.from(`.square${i}`, {
+       duration: .5,
+       y: "-60vh",
+       display: "flex",
+     })
+     .from(`.square${i} .childSquare`, {
+       duration: .5,
+       x: "0",
+       y: "0",
+       backgroundColor: 'hsl(329, 57%, 25%)',
+       rotate: "random(-45, 45, 2)",
+     })
+     .to(`.square${i}`, {
+       backgroundColor: 'hsl(329, 57%, 25%)',
+       duration: 1,
+       height: "150px",
+       width: "150px",
+       y: "55vh",
+       ease: "bounce"
+      })
+      .add("explode")
+      .to(`.square${i}`, {
+        duration: .2,
+        backgroundColor: 'hsla(329, 57%, 25%, 0%)'
+      }, "explode")
+      .to(`.square${i} #part1`, {
+        duration:.5,
+        y: "-=50vh",
+        x: "-=30vh",
+        backgroundColor: 'hsla(329, 57%, 25%, 0%)',
+      }, "explode")
+      .to(`.square${i} #part2`, {
+        duration:.5,
+        y: "-=50vh",
+        x: "+=10vh",
+        backgroundColor: 'hsla(329, 57%, 25%, 0%)',
+      }, "explode")
+      .to(`.square${i} #part3`, {
+        duration:.5,
+        y: "-=50vh",
+        x: "-=60vh",
+        backgroundColor: 'hsla(329, 57%, 25%, 0%)',
+      }, "explode")
+      .to(`.square${i} #part4`, {
+        duration:.5,
+        y: "-=50vh",
+        x: "+=30vh",
+        backgroundColor: 'hsla(329, 57%, 25%, 0%)',
+      }, "explode")
+     }
+   }

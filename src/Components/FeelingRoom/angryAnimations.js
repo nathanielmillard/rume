ed9d608728335e26bb2a90fac8c8a4016f2e5a23
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-03 13:27:52 -0700

    Add angryFeelingAnimation3 function

--- src/Components/FeelingRoom/angryAnimations.js
+++ src/Components/FeelingRoom/angryAnimations.js
@@ -109,3 +109,57 @@ export const angryFeelingAnimation2 = () => {
       }, "explode")
      }
    }
+export const angryFeelingAnimation3 = () => {
+  let angryTimeline3 = gsap.timeline({repeat: 10, delay: 1});
+  for (var i = 2; i <= 12; i++) {
+    angryTimeline3.from(`.square${i}`, {
+      duration: .5,
+      y: "-60vh",
+      display: "flex",
+    })
+    .from(`.square${i} .childSquare`, {
+      duration: .5,
+      x: "0",
+      y: "0",
+      backgroundColor: 'hsl(329, 57%, 25%)',
+      rotate: "random(-45, 45, 2)",
+    })
+    .to(`.square${i}`, {
+      backgroundColor: 'hsl(329, 57%, 25%)',
+      duration: 1,
+      height: "150px",
+      width: "150px",
+      y: "55vh",
+      ease: "bounce"
+     })
+     .add("explode")
+     .to(`.square${i}`, {
+       duration: .2,
+       backgroundColor: 'hsla(329, 57%, 25%, 0%)'
+     }, "explode")
+     .to(`.square${i} #part1`, {
+       duration:.5,
+       y: "-=50vh",
+       x: "-=30vh",
+       backgroundColor: 'hsla(329, 57%, 25%, 0%)',
+     }, "explode")
+     .to(`.square${i} #part2`, {
+       duration:.5,
+       y: "-=50vh",
+       x: "+=10vh",
+       backgroundColor: 'hsla(329, 57%, 25%, 0%)',
+     }, "explode")
+     .to(`.square${i} #part3`, {
+       duration:.5,
+       y: "-=50vh",
+       x: "-=60vh",
+       backgroundColor: 'hsla(329, 57%, 25%, 0%)',
+     }, "explode")
+     .to(`.square${i} #part4`, {
+       duration:.5,
+       y: "-=50vh",
+       x: "+=30vh",
+       backgroundColor: 'hsla(329, 57%, 25%, 0%)',
+     }, "explode")
+   }
+ }

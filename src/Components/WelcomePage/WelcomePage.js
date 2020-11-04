8b7ed83527d60e7c50bcc2d3f03388f2e78b4bba
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-03 17:33:36 -0700

    Add logo animation to welcome page

--- src/Components/WelcomePage/WelcomePage.js
+++ src/Components/WelcomePage/WelcomePage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './WelcomePage.scss'
+import gsap from 'gsap'
+
 
 const WelcomePage = () => {
+  const animate = () => {
+    gsap.to(".logo", {duration: 3, fontSize: "30vw", ease: "power2", opacity: 100})
+  }
   return (
     <section className="welcomeWrapper">
+      {animate()}
       <h1 className="logo" data-testid='appTitle'>Rume</h1>
       <h2 data-testid='appDirections'>Click the door below to enter the lobby</h2>
     </section>

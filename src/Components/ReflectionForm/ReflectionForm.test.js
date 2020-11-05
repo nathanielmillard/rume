47b012e4c8183d4a6d9204374c4f84c0855c3fa6
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-04 18:53:12 -0700

    Add unit and integration tests

--- src/Components/ReflectionForm/ReflectionForm.test.js
+++ src/Components/ReflectionForm/ReflectionForm.test.js
@@ -1,10 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import ReflectionForm from './ReflectionForm';
-import {MemoryRouter} from 'react-router-dom'
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom'
+import { postAReflection } from '../../apiCalls.js'
+jest.mock('../../apiCalls.js')
 
 describe('ReflectionForm', ()=>{
   describe('Unit Tests', ()=>{
-    test('Should render a footer on the ReflectionForm', () => {
+    test('Should render a ReflectionForm', () => {
       render(
         <MemoryRouter>
           <ReflectionForm
@@ -12,9 +15,55 @@ describe('ReflectionForm', ()=>{
           />
         </MemoryRouter>
       );
-      const button = screen.getByTestId("linkToAllReflections");
-      expect(button).toBeInTheDocument();
+      const libraryButton = screen.getByTestId("linkToAllReflections");
+      expect(libraryButton).toBeInTheDocument();
+      const submitButton = screen.getByText("Save This Reflection");
+      expect(submitButton).toBeInTheDocument();
+      const reflectionForm = screen.getByTestId("reflectionForm");
+      expect(reflectionForm).toBeInTheDocument();
     });
-
+  })
+  describe('Integration Tests', ()=>{
+    test('Should reset the input when a user types a reflection and submits it', () => {
+      render(
+        <MemoryRouter>
+          <ReflectionForm
+            mood='Fine'
+          />
+        </MemoryRouter>
+      );
+      const libraryButton = screen.getByTestId("linkToAllReflections");
+      expect(libraryButton).toBeInTheDocument();
+      const submitButton = screen.getByText("Save This Reflection");
+      expect(submitButton).toBeInTheDocument();
+      const reflectionForm = screen.getByTestId("reflectionForm");
+      expect(reflectionForm).toBeInTheDocument();
+      const input = screen.getByTestId("reflectionArea");
+      userEvent.type(input, "Feeling fine!")
+      expect(input.value).toBe("Feeling fine!");
+      userEvent.click(submitButton);
+      expect(input.value).toBe("");
+    });
+    test('Should post a reflection', () => {
+      render(
+        <MemoryRouter>
+          <ReflectionForm
+            mood='Fine'
+          />
+        </MemoryRouter>
+      );
+      const libraryButton = screen.getByTestId("linkToAllReflections");
+      expect(libraryButton).toBeInTheDocument();
+      const submitButton = screen.getByText("Save This Reflection");
+      expect(submitButton).toBeInTheDocument();
+      const reflectionForm = screen.getByTestId("reflectionForm");
+      expect(reflectionForm).toBeInTheDocument();
+      const input = screen.getByTestId("reflectionArea");
+      const date = new Date()
+      userEvent.type(input, "Feeling fine!")
+      expect(input.value).toBe("Feeling fine!");
+      userEvent.click(submitButton);
+      expect(postAReflection).toHaveBeenCalled()
+    });
   })
 })

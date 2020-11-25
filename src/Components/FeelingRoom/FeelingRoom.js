84af29cbeff72226008679df8dd2157bf7f2f168
Brigette Doelp <brigette.doelp@gmail.com>
2020-11-25 13:38:12 -0700

    Refactor one hex to use PNG instead of CSS element

--- src/Components/FeelingRoom/FeelingRoom.js
+++ src/Components/FeelingRoom/FeelingRoom.js
@@ -22,7 +22,7 @@ import pause from '../../Assets/pause.svg'
 
 import hexoutline1 from '../../Assets/hexoutline1.png';
 import hexoutline2 from '../../Assets/hexoutline2.png';
-import hexoutline3 from '../../Assets/hexoutline1=3.png';
+import hexoutline3 from '../../Assets/hexoutline3.png';
 import hexsolid1 from '../../Assets/hexsolid1.png';
 import hexsolid2 from '../../Assets/hexsolid2.png';
 
@@ -80,30 +80,10 @@ class FeelingRoom extends Component {
     )
   }
   createAnxiousRoom = () => {
-    anxiousAnimation('hex1', .74, 1, 1.8, .90)
-    anxiousAnimation('#hex2', .5, 1, .7, .45)
-    anxiousAnimation('#hex3', .6, .7, 3.4, 2)
-    anxiousAnimation('#hex4', .78, .2, .2, .75)
-    anxiousAnimation('#hex5', .67, .1, .1, 0)
-    anxiousAnimation('#hex6', .74, 1, 1.8, .90)
-    anxiousAnimation('#hex7', .86, 1, 1.8, .55)
-    anxiousAnimation('#hex8', .8, .5, 2, 1.5)
-    anxiousAnimation('#hex9', .5, 1, .7, .45)
-    anxiousAnimation('#hex10', .7, 1, 2.5, 2.5)
-    anxiousAnimation('#hex11', .49, .5, .3, 2.5)
+    anxiousAnimation('#hex1', .74, 1, 1.8, .90)
     return (
       <section className='anxiousRoom'>
-        <div class='hexagon' id='hex1'></div>
-        <div class='hexagon' id='hex2'></div>
-        <div class='hexagon' id='hex3'></div>
-        <div class='hexagon' id='hex4'></div>
-        <div class='hexagon' id='hex5'></div>
-        <div class='hexagon' id='hex6'></div>
-        <div class='hexagon' id='hex7'></div>
-        <div class='hexagon' id='hex8'></div>
-        <div class='hexagon' id='hex9'></div>
-        <div class='hexagon' id='hex10'></div>
-        <div class='hexagon' id='hex11'></div>
+        <img id='hex1' src={hexoutline1} />
       </section>
     )
   }
@@ -280,6 +260,30 @@ class FeelingRoom extends Component {
     )
   }
 }
+
+// <div class='hexagon' id='hex1'></div>
+// <div class='hexagon' id='hex2'></div>
+// <div class='hexagon' id='hex3'></div>
+// <div class='hexagon' id='hex4'></div>
+// <div class='hexagon' id='hex5'></div>
+// <div class='hexagon' id='hex6'></div>
+// <div class='hexagon' id='hex7'></div>
+// <div class='hexagon' id='hex8'></div>
+// <div class='hexagon' id='hex9'></div>
+// <div class='hexagon' id='hex10'></div>
+// <div class='hexagon' id='hex11'></div>
+
+// anxiousAnimation('#hex1', .74, 1, 1.8, .90)
+// anxiousAnimation('#hex2', .5, 1, .7, .45)
+// anxiousAnimation('#hex3', .6, .7, 3.4, 2)
+// anxiousAnimation('#hex4', .78, .2, .2, .75)
+// anxiousAnimation('#hex5', .67, .1, .1, 0)
+// anxiousAnimation('#hex6', .74, 1, 1.8, .90)
+// anxiousAnimation('#hex7', .86, 1, 1.8, .55)
+// anxiousAnimation('#hex8', .8, .5, 2, 1.5)
+// anxiousAnimation('#hex9', .5, 1, .7, .45)
+// anxiousAnimation('#hex10', .7, 1, 2.5, 2.5)
+// anxiousAnimation('#hex11', .49, .5, .3, 2.5)
 
 
 export default FeelingRoom
